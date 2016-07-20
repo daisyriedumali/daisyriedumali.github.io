909f0c3b54e186cbad8d0c1e94e3f7f2fcb05761
Daisyrie Dumali <daisyrie.dumali@goabroad.com>
2016-07-20 23:25:13 +0800

    added constants.js

---
+++ js/constants.js
@@ -0,0 +1,89 @@
+var SHIRTWEBSITES = [
+						{
+							name: "Teespring",
+							url: "teespring.com"
+						},
+						{
+							name: "Teezily",
+							url: "teezily.com"
+						},
+						{
+							name: "Fabrily",
+							url: "fabrily.com"
+						},
+						{
+							name: "Teechip",
+							url: "teechip.com"
+						},
+						{
+							name: "Sunfrog Shirt",
+							url: "sunfrogshirts.com"
+						}
+					];
+
+
+var FBTIME = [
+			 	{
+			 		time: "Any Time",
+			 		stringVal: ""
+			 	},
+			 	{
+			 		time: "Today",
+			 		stringVal: "/today/date/stories"
+			 	},
+			 	{
+			 		time: "Yesterday",
+			 		stringVal: "/yesterday/date/stories"
+			 	},
+			 	{
+			 		time: "This week",
+			 		stringVal: "/this-week/date/stories"
+			 	},
+			 	{
+			 		time: "Last week",
+			 		stringVal: "/last-week/date/stories"
+			 	},
+			 	{
+			 		time: "This month",
+			 		stringVal: "/this-month/date/stories"
+			 	},
+			 	{
+			 		time: "Last month",
+			 		stringVal: "/last-month/date/stories"
+			 	},
+			 	{
+			 		time: "This year",
+			 		stringVal: "/this-year/date/stories"
+			 	},
+			 	{
+			 		time: "Last year",
+			 		stringVal: "/last-year/date/stories"
+			 	}
+			 ];
+
+var POSTTYPE = [
+			   		{
+			   			post: "All Posts",
+			   			stringVal: "/intersect"
+			   		},
+			   		{
+			   			post: "Live Posts",
+			   			stringVal: "/intersect/stories-live"
+			   		},
+			   		{
+			   			post: "Page Posts",
+			   			stringVal: "/intersect/stories-publishers"
+			   		},
+			   		{
+			   			post: "Photo Posts",
+			   			stringVal: "/photo/stories/intersect"
+			   		},
+			   		{
+			   			post: "Video Posts",
+			   			stringVal: "/video/stories/intersect"
+			   		},
+			   		{
+			   			post: "Shared Posts",
+			   			stringVal: "/share/stories/intersect"
+			   		}
+			   ];
